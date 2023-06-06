da22b49fa83f10b4372c3228678e98c7b236547f
Vengamo <133201077+vengamo@users.noreply.github.com>
2023-06-06 11:35:27 +0300

    Delete example directory

--- example/lib/models/message.dart
+++
@@ -1,8 +0,0 @@
-class Message {
-  int id;
-  String senderId;
-  String receiverId;
-  String text;
-  String status;
-  Message(this.id, this.senderId, this.receiverId, this.text, this.status);
-}
