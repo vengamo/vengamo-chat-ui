26b845074985de74912bbb61022ea29fe690dc90
Eric Weeb <133201077+vengamo@users.noreply.github.com>
2023-08-01 10:44:12 +0300

    updated example page

--- example/README.md
+++ example/README.md
@@ -1,67 +1,16 @@
-# Vengamo Chat UI
+# vengamo_chat
 
-Vengamo Chat UI is a Flutter project that combines the user interface elements of WhatsApp, Telegram, and Facebook Messenger in one place. It offers a seamless and intuitive chat experience inspired by these popular messaging applications.
-
-## Features
-
-- Familiar UI elements from WhatsApp, Telegram, and Facebook Messenger.
-- Group chats with support for multiple participants.
-- Real-time message updates and notifications.
-- Rich media sharing, including photos, videos, and files.
-- Emojis and stickers for expressive conversations.
-- Voice and video calling functionalities (to be implemented).
-- Customizable themes to personalize the chat interface.
-
-## Screenshots
-
-![image1](https://github.com/vengamo/vengamo-chat-ui/assets/133201077/6d4d6d2b-0433-48b7-ab0e-0d8136d96f58)
+A new Flutter project.
 
 ## Getting Started
 
-To get started with Vengamo Chat UI, follow these steps:
+This project is a starting point for a Flutter application.
 
-1. Ensure that you have Flutter installed on your system. For installation instructions, refer to the [Flutter official documentation](https://flutter.dev/docs/get-started/install).
+A few resources to get you started if this is your first Flutter project:
 
-2. Clone this repository to your local machine:
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
 
-   ```
-   git clone https://github.com/vengamo/vengamo-chat-ui.git
-   ```
-
-3. Change to the project directory:
-
-   ```
-   cd vengamo-chat-ui
-   ```
-
-4. Fetch the project dependencies:
-
-   ```
-   flutter pub get
-   ```
-
-5. Run the app on a connected device or emulator:
-
-   ```
-   flutter run
-   ```
-
-   This will start the Vengamo Chat UI app on your device.
-
-## Customization
-
-Vengamo Chat UI allows for customization to fit your preferences:
-
-- Modify the chat bubble design and styles in the codebase to match your desired look and feel.
-- Explore the theme options to change the overall visual appearance of the app.
-- Customize notification settings and sounds to suit your preferences.
-
-## Contributions
-
-Contributions to the Vengamo Chat UI project are welcome! If you find any issues or have suggestions for improvements, feel free to open an issue or submit a pull request. Please follow the project's code of conduct.
-
-## License
-
-The Vengamo Chat UI project is licensed under the [MIT License](LICENSE).
-
-Feel free to use this README template as a starting point and add more details as needed to provide a comprehensive overview of your Vengamo Chat UI project.
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,58 +1,331 @@
 import 'package:flutter/material.dart';
-import 'package:vengamo_chat/provider/MessageProvider.dart';
-import 'package:vengamo_chat/provider/sendButtonProvider.dart';
-import 'package:vengamo_chat/theme/AppColor.dart';
-import 'components/ChatMessages.dart';
-import 'components/textInput.dart';
-import 'models/message.dart';
-import 'package:line_awesome_flutter/line_awesome_flutter.dart';
-import 'package:provider/provider.dart';
+import 'package:vengamo_chat_ui/vengamo_chat_ui.dart';
+import 'package:vengamo_chat_ui/theme/app_color.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => SendButtonProvider(),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => MessagesProvider(),
-        )
-      ],
-      child: MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Chat App',
-      home: MyHomePage(title: 'Vengamo chat UI'),
-    ));
+      title: 'James Wagon',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const VengamoChatScreen(),
+    );
   }
 }
 
-class MyHomePage extends StatelessWidget {
- MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+class VengamoChatScreen extends StatelessWidget {
+  const VengamoChatScreen({super.key});
+
+  String getTime() {
+    int hour = DateTime.now().hour;
+    int min = DateTime.now().minute;
+    String hRes = hour <= 9 ? '0$hour' : hour.toString();
+    String mRes = min <= 9 ? '0$min' : min.toString();
+    return '$hRes:$mRes';
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: AppColors.primarycolor,
-        title: Text(title),
+        title: const Text('James Wagon'),
       ),
-      body: Column(
-        children: const [
-          Expanded(
-            child: ChatMessages()
-            ), 
-          TextInputField()
-        ],
-      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+              color: AppColors.backgroundColor,
+            ),
+ 
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20, top: 2),
+            child: Column(
+              children: [
+
+              VengamoChatUI(
+              senderBgColor: AppColors.softGreenColor,
+              receiverBgColor: AppColors.white,
+              isSender: true,
+              isNextMessageFromSameSender: false,
+              time: getTime(),
+              isAudio: true,
+              audioSource: 'http://www.uscis.gov/files/nativedocuments/Track%2093.mp3',
+              timeLabelColor: AppColors.iconColor,
+              pointer: true,
+              ack: Image.asset(
+                'assets/images/seen.png',
+                height: 12,
+                width: 12,
+              ),
+            ),
+            const SizedBox(height: 10,),
+            VengamoChatUI(
+              senderBgColor: AppColors.softGreenColor,
+              receiverBgColor: AppColors.white,
+              isSender: false,
+              isNextMessageFromSameSender: false,
+              time: getTime(),
+              isAudio: true,
+              audioSource: 'https://p.scdn.co/mp3-preview/c2bca95698cc381f0a1a9111095156d15ebd4698',
+              timeLabelColor: AppColors.iconColor,
+              pointer: true,
+              ack: Image.asset(
+                'assets/images/seen.png',
+                height: 12,
+                width: 12,
+              ),
+            ),
+            // const SizedBox(height: 10,),
+            //     VengamoChatUI(
+            //   senderBgColor: AppColors.softGreenColor,
+            //   receiverBgColor: AppColors.white,
+            //   timeLabelColor: AppColors.darkModeBackgroundColor,
+            //   isSender: false,
+            //   isNextMessageFromSameSender: false,
+            //   imgUrl : 'https://static.wikia.nocookie.net/frstarwars/images/4/43/TheChild-Fathead.png/revision/latest?cb=20201101090715',
+            //   caption : "check this out! Why don't skeletons fight each other? They don't have the guts!",
+            //   time: getTime(),
+            //   pointer: true, 
+            //   ack: Image.asset(
+            //     'assets/images/seen.png',
+            //     height: 12,
+            //     width: 12,
+            //   ),
+            // ),
+            const SizedBox(height: 10,),
+            VengamoChatUI(
+              senderBgColor: AppColors.softGreenColor,
+              receiverBgColor: AppColors.white,
+              isSender: false,
+              timeLabelColor: AppColors.white,
+              isNextMessageFromSameSender: false,
+              imgUrl : 'https://cdn.wallpapersafari.com/28/2/vrIzJD.jpg',
+              time: getTime(),
+              pointer: true, 
+              ack: Image.asset(
+                'assets/images/seen.png',
+                height: 12,
+                width: 12,
+              ),
+            ),
+            const SizedBox(height: 10,),
+            VengamoChatUI(
+              senderBgColor: AppColors.softGreenColor,
+              receiverBgColor: AppColors.white,
+              isSender: false,
+              timeLabelColor: AppColors.white,
+              isNextMessageFromSameSender: false,
+              imgUrl : 'https://weellu.s3.us-east-2.amazonaws.com/test/kYFhiApyyVRX.jpeg',
+              time: getTime(),
+              pointer: true, 
+              ack: Image.asset(
+                'assets/images/seen.png',
+                height: 12,
+                width: 12,
+              ),
+            ),
+               VengamoChatUI(
+              senderBgColor: AppColors.softGreenColor,
+              receiverBgColor: AppColors.white,
+              isSender: true,
+              isNextMessageFromSameSender: false,
+              time: getTime(),
+              timeLabelColor : AppColors.softBlackcolor,
+              text: "Yolla ✋ Hey, guess what?",
+              pointer: true, 
+              ack: const Icon(
+                      Icons.check,
+                      color: AppColors.iconColor, // You can customize the color here
+                    size: 13, // You can customize the size here
+              ),
+            ),
+            VengamoChatUI(
+              senderBgColor: AppColors.softGreenColor,
+              receiverBgColor: AppColors.white,
+              isSender: true,
+              isNextMessageFromSameSender: true,
+              time: getTime(),
+              timeLabelColor : AppColors.softBlackcolor,
+              text: "What?",
+              pointer: false, 
+              ack: Image.asset(
+                'assets/images/double_tick_grey.png',
+                height: 13,
+                width: 13,
+              ),
+            ),
+            VengamoChatUI(
+              senderBgColor: AppColors.softGreenColor,
+              receiverBgColor: AppColors.white,
+              isSender: false,
+              isNextMessageFromSameSender: false,
+              time: getTime(),
+              text: "I invented a new word!.",
+              pointer: true, 
+              ack: Image.asset(
+                'assets/images/double_tick_green.png',
+                height: 14,
+                width: 15,
+              ),
+            ),
+            const SizedBox(height: 5,),
+
+            VengamoChatUI(
+              senderBgColor: AppColors.softGreenColor,
+              receiverBgColor: AppColors.white,
+              timeLabelColor: AppColors.darkModeBackgroundColor,
+              isSender: true,
+              isNextMessageFromSameSender: false,
+              imgUrl : 'https://wallpaperaccess.com/full/1248267.jpg',
+              caption: "Why did the sun never want to join the galaxy's talent show? Because it didn't want to be a star performer, it preferred to shine solo",
+              time: getTime(),
+              pointer: true, 
+              ack: Image.asset(
+                'assets/images/seen.png',
+                height: 12,
+                width: 12,
+              ),
+            ),
+            VengamoChatUI(
+              senderBgColor: AppColors.softGreenColor,
+              receiverBgColor: AppColors.white,
+              isSender: false,
+              isNextMessageFromSameSender: true,
+              time: getTime(),
+              text: "Hold on, a sec, let me pick this call?",
+              pointer: false, 
+              ack: Image.asset(
+                'assets/images/double_tick_green.png',
+                height: 14,
+                width: 15,
+              ),
+            ),
+            VengamoChatUI(
+              senderBgColor: AppColors.softGreenColor,
+              receiverBgColor: AppColors.white,
+              isSender: false,
+              isNextMessageFromSameSender: true,
+              time: getTime(),
+              text: "Plagiarism! 💫 😂 ",
+              pointer: false, 
+              ack: Image.asset(
+                'assets/images/double_tick_green.png',
+                height: 14,
+                width: 15,
+              ),
+            ),
+            VengamoChatUI(
+              senderBgColor: AppColors.softGreenColor,
+              receiverBgColor: AppColors.white,
+              isSender: true,
+              isNextMessageFromSameSender: false,
+              time: getTime(),
+              text: "Plagiarism!? 😂.",
+              pointer: true, 
+              ack: Image.asset(
+                'assets/images/sent.png',
+                height: 12,
+                width: 12,
+              ),
+            ),
+           
+             VengamoChatUI(
+              senderBgColor: AppColors.softGreenColor,
+              receiverBgColor: AppColors.white,
+              isSender: true,
+              isNextMessageFromSameSender: true,
+              time: getTime(),
+              text: "Thanks! I thought you'd like it 🤪",
+              pointer: false, 
+              ack: Image.asset(
+                'assets/images/seen.png',
+                height: 12,
+                width: 12,
+              ),
+            ), VengamoChatUI(
+              senderBgColor: AppColors.softGreenColor,
+              receiverBgColor: AppColors.white,
+              isSender: true,
+              isNextMessageFromSameSender: true,
+              time: getTime(),
+              text: "Why don't scientists trust atoms? Because they make up everything! If you'd like to hear more jokes or have any other questions, feel free to ask.😂🤪",
+              pointer: false, 
+              ack: Image.asset(
+                'assets/images/seen.png',
+                height: 12,
+                width: 12,
+              ),
+            ),
+        
+            VengamoChatUI(
+              senderBgColor: AppColors.softGreenColor,
+              receiverBgColor: AppColors.white,
+              isSender: false,
+              isNextMessageFromSameSender: false,
+              time: getTime(),
+              text: "check this out! Why don't skeletons fight each other? They don't have the guts!",
+              pointer: true, 
+              ack: Image.asset(
+                'assets/images/seen.png',
+                height: 12,
+                width: 12,
+              ),
+            ),
+            
+            const SizedBox(height: 5,),
+
+            VengamoChatUI(
+              senderBgColor: AppColors.softGreenColor,
+              receiverBgColor: AppColors.white,
+              isSender: true,
+              isNextMessageFromSameSender: false,
+              imgUrl : 'https://cache.marieclaire.fr/data/photo/w1000_ci/1ju/sean-connery-james-bond.jpg',
+              caption: "Why did the sun never want to join the galaxy's talent show? Because it didn't want to be a star performer, it preferred to shine solo",
+              time: getTime(),
+              pointer: true, 
+              ack: Image.asset(
+                'assets/images/seen.png',
+                height: 12,
+                width: 12,
+              ),
+            ),
+
+            VengamoChatUI(
+              senderBgColor: AppColors.softGreenColor,
+              receiverBgColor: AppColors.white,
+              isSender: true,
+              caption: "You'd like it 🤪",
+              isNextMessageFromSameSender: false,
+              imgUrl : 'https://wallpapers.com/images/high/peaceful-relaxing-vhbesnvs3ylhpuvk.webp',
+              time: getTime(),
+              timeLabelColor: AppColors.iconColor,
+              pointer: false,
+              ack: Image.asset(
+                'assets/images/seen.png',
+                height: 12,
+                width: 12,
+              ),
+            ),
+
+           
+        
+              ],
+            ),
+          ),
+        ),
+      ), // Use Vengamo Chat UI component here
     );
   }
 }
